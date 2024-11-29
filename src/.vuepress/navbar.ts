3438e2b673b0fcef27b6cdf5297562fce92792da
niuyafeng777 <1625099286@qq.com>
2024-11-29 12:39:50 +0800

    feature：添加javaDoc

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -4,4 +4,5 @@ export default navbar([
   "/",
   "/Java/",
   "/JavaSE/",
+  "/javaDocs/",
 ]);
--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -2,5 +2,6 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
     '/Java/': 'structure',
-    '/JavaSE/': 'structure'
+    '/JavaSE/': 'structure',
+    '/javaDocs/': 'structure',
 });
